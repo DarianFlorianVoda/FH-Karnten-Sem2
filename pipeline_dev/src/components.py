383168aec18fa71652d5515defe1145398d00d14
Voda Darian <darian.voda00@e-uvt.ro>
2023-04-26 19:23:27 +0200

    -DE Part 2 - 26.04.2023

--- pipeline_dev/src/components.py
+++ pipeline_dev/src/components.py
@@ -1,5 +1,6 @@
 from base import PipelineComponent
 from data import DataPiece
+import tinydb as tdb
 
 
 class SimpleOut(PipelineComponent):
@@ -15,6 +16,43 @@ class CleanData(PipelineComponent):
 
 
 class ChangeIndex(PipelineComponent):
+    def __init__(self, col_name) -> None:
+        self.name = col_name
+
     def run(self, input: DataPiece):
-        input.frame['id'] = input.frame.index.values.tolist()
+        input.frame.set_index(self.name, inplace=True)
         return input
+
+
+class MultiplyCols(PipelineComponent):
+    def __init__(self, col1: str, col2: str, result_name: str) -> None:
+        self.col1 = col1
+        self.col2 = col2
+        self.name = result_name
+
+    def run(self, input: DataPiece):
+        input.frame[self.name] = input.frame[self.col1] * input.frame[self.col2]
+        return input
+
+
+class FilterRows(PipelineComponent):
+    def __init__(self, cname: str, threshold: float):
+        self.threshold = threshold
+        self.cname = cname
+
+    def run(self, input: DataPiece):
+        input.frame = input.frame.loc[input.frame[self.cname] > self.threshold]
+        return input
+
+
+class StoreToyData(PipelineComponent):
+    def __init__(self, path: str):
+        self.path = path
+
+    def run(self, input: DataPiece):
+        with tdb.TinyDB(self.metadata) as db:
+            src_tab = db.table("data_source")
+            src_tab.insert(input.meta)
+
+            customer_tab = db.table("customers")
+            customer_tab.insert(input.frame.to_dict())
--- pipeline_dev/src/main.py
+++ pipeline_dev/src/main.py
@@ -1,17 +1,22 @@
 from pathlib import Path
-
+import yaml
 from reader import CsvReader
-from components import CleanData, SimpleOut, ChangeIndex
+from components import CleanData, SimpleOut, ChangeIndex,  MultiplyCols, FilterRows, StoreToyData
 
 _IN_FILE_PATH = str(Path(__file__).parents[1] / "input_data.csv")
+_OUT_FILE_PATH = str(Path(__file__).parents[1] / "tdb.json")
 
 if __name__ == "__main__":
     csv_reader = CsvReader(sep=";", col_names=["name", "id", "pcs", "factor"])
     outprint = SimpleOut()
     cleaner = CleanData()
-    indexer = ChangeIndex()
+    indexer = ChangeIndex("id")
+    mul = MultiplyCols("pcs", "factor", "weight")
+    filt = FilterRows("pcs", 3)
 
     in_data = csv_reader.run(_IN_FILE_PATH)
     cleaned_data = cleaner.run(in_data)
     indexed_data = indexer.run(in_data)
-    outprint.run(in_data)
+    mul_data = mul.run(indexed_data)
+    filt_data = filt.run(in_data)
+    outprint.run(filt_data)
