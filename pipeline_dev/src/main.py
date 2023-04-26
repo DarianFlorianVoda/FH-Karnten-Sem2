cced40aa873cce3ade3b31c75dc65c739003d408
Voda Darian <darian.voda00@e-uvt.ro>
2023-04-26 19:46:51 +0200

    -DE Part 2 - 26.04.2023 (2)

--- pipeline_dev/src/main.py
+++ pipeline_dev/src/main.py
@@ -6,13 +6,18 @@ from components import CleanData, SimpleOut, ChangeIndex,  MultiplyCols, FilterR
 _IN_FILE_PATH = str(Path(__file__).parents[1] / "input_data.csv")
 _OUT_FILE_PATH = str(Path(__file__).parents[1] / "tdb.json")
 
+
 if __name__ == "__main__":
-    csv_reader = CsvReader(sep=";", col_names=["name", "id", "pcs", "factor"])
+    with open("cfg.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    csv_reader = CsvReader(**config['CsvReader'])
     outprint = SimpleOut()
     cleaner = CleanData()
-    indexer = ChangeIndex("id")
-    mul = MultiplyCols("pcs", "factor", "weight")
-    filt = FilterRows("pcs", 3)
+    indexer = ChangeIndex(**config['ChangeIndex'])
+    mul = MultiplyCols(**config['MultiplyCols'])
+    filt = FilterRows(**config['FilterRows'])
+    writedb = StoreToyData(**config['StoreToyData'])
 
     in_data = csv_reader.run(_IN_FILE_PATH)
     cleaned_data = cleaner.run(in_data)
